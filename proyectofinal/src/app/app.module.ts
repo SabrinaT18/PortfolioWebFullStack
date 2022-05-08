732164af7a64830b1484cd4217db10fdac4f7346
SabrinaT18 <96924905+SabrinaT18@users.noreply.github.com>
2022-05-08 14:09:27 -0300

    angular

---
+++ proyectofinal/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponentesComponent } from './componentes/componentes.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { NgCircleProgressModule } from 'ng-circle-progress' ;
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponentesComponent,
+    EncabezadoComponent,
+    AcercadeComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    SkillsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
